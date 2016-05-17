245edf3d67bc08d23e6980281c573e7a76430f86
Federico Minzoni <fminzoni@enter.it>
2016-05-17 23:39:18 +0200

    first commit

---
+++ dotnetapp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Npgsql;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var conn = new NpgsqlConnection("Host=db;Username=postgres;Database=postgres"))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    // init db
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS greetings (text VARCHAR(255))";
+                    cmd.ExecuteNonQuery();
+                    
+                    // insert first greeting
+                    cmd.CommandText = "INSERT INTO greetings (text) VALUES ('Hello Docker')";
+                    cmd.ExecuteNonQuery();
+
+                    // try to retrieve data
+                    cmd.CommandText = "SELECT text FROM greetings";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                        }
+                    }
+                    
+                }     
+            }
+        }
+        
+    }
+}
